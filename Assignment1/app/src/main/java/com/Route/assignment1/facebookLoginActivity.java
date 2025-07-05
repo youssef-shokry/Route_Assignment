b974876c157cd8bdf70d535043905894382d9ef0
Youssef Ahmed Ali <youssefshokry98@gmail.com>
2025-07-05 14:30:15 +0300

    assignment1 code

---
+++ Assignment1/app/src/main/java/com/Route/assignment1/facebookLoginActivity.java
@@ -0,0 +1,15 @@
+package com.Route.assignment1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class facebookLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_facebook);
+    }
+}
